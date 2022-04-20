b1185b74e3961ab9561ae4ac13f9421f66ddbe3e
Ivaylo Tsankov <itsankoff@gmail.com>
2022-04-20 23:19:35 +0300

    * Init version of the tool

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello slowloris")
+	var url *string = flag.String("url", "", "URL to perform attack")
+	var count *int64 = flag.Int64("count", int64(10), "Number of parallel workers")
+
+	flag.Parse()
+	if !flag.Parsed() {
+		Help()
+		return
+	}
+
+	if *url == "" {
+		Help()
+		return
+	}
+
+	fmt.Println("slowloris...")
+	fmt.Printf("url: %s, count: %d\n", *url, *count)
+	if err := Zoo(Config{
+		URL:   *url,
+		Count: *count,
+	}); err != nil {
+		fmt.Println(err)
+	}
+
+	return
 }
