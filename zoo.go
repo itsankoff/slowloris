bd90cfa19106d6745cb1831e12957eccf04c9fe8
Ivaylo Tsankov <itsankoff@gmail.com>
2022-04-25 13:08:46 +0300

    * Support tls connections + some refactoring

--- zoo.go
+++ zoo.go
@@ -18,20 +18,20 @@ type Options struct {
 }
 
 // Zoo performs a distributed slowloris attack based on the given parameters
-func Zoo(c Config) error {
+func Zoo(o Options) error {
 	var ctx context.Context
 	ctx = context.Background()
-	if c.Timeout != 0 {
-		ctx, _ = context.WithTimeout(ctx, c.Timeout)
+	if o.Timeout != 0 {
+		ctx, _ = context.WithTimeout(ctx, o.Timeout)
 	}
 
 	wg := sync.WaitGroup{}
-	for i := int64(0); i < c.Count; i++ {
+	for i := int64(0); i < o.Count; i++ {
 		wg.Add(1)
 		go func(index int64) {
 			defer wg.Done()
 			fmt.Printf("slowloris %d\n", index)
-			if err := Slowloris(ctx, index, c); err != nil {
+			if err := Slowloris(ctx, index, o); err != nil {
 				fmt.Printf("slowloris %d received err: %s\n", index, err)
 				return
 			}
